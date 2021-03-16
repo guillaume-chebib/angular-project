59951a49070c42a8ee67e31b7a70ba8f0671aba9
guillaume-chebib <guillaume.chebib@hotmail.fr>
2021-03-16 11:07:29 +0100

    2 files modified

--- src/app/festivals/festivals.component.ts
+++ src/app/festivals/festivals.component.ts
@@ -10,6 +10,8 @@ export class FestivalsComponent implements OnInit {
 
   public festivals;
   public nbfestivals;
+  public festivalToAdd;
+  public nbmodif;
 
   ngOnInit(): void {
     this.festivals = [
@@ -17,6 +19,17 @@ export class FestivalsComponent implements OnInit {
       { name: "FJM2018", tables: 80 },
       { name: "FJM2019", tables: 110 }
     ];
-    this.nbfestivals = this.festivals.length
+    this.nbfestivals = this.festivals.length;
+    this.festivalToAdd = { name: "FJM2021", tables: 200 };
+    this.nbmodif = 0;
+  }
+
+  addFestival() : void {
+    this.festivals.push(this.festivalToAdd) 
+  }
+
+  changeFestival() : void {
+    this.nbmodif +=1
+    this.festivals[0]["name"] = this.festivals[0]["name"] + this.nbmodif
   }
 }
